63e9e90b7acec9266896ada1cb0a147050c1ffa0
Mariam-90 <83160379+Mariam-90@users.noreply.github.com>
2025-11-09 21:56:21 +0200

    check letter

--- main_game.py
+++ main_game.py
@@ -1,1 +1,9 @@
 print("Welcome to the Hangman game!")
+
+def  check_letter():
+    letter = input("Enter a letter: ")
+    if letter.isalpha() and len(letter) == 1:
+        return letter
+    return False
+
+print(check_letter())
